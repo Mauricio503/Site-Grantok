814f4d11f689a0cb67c591752c2397e71f82b4b3
Mauricio503 <mauriciogspagnol@gmail.com>
2017-11-28 11:26:28 -0200

    Desenvolvendo os botões dos produtos

---
+++ Site grantok.com.br/js/troca-imagens.js
@@ -0,0 +1,25 @@
+function mudaImagem(i) {    
+
+	if (i == 1) {
+        document.getElementById("imagem-principal").src="img/hdf branca.png";   
+    } else if (i == 2) {       
+    	document.getElementById("imagem-principal").src="img/hdf frisada.png";        
+    } else if (i == 3){
+    	document.getElementById("imagem-principal").src="img/angelim lisa.png"
+    } else if (i == 4){
+    	document.getElementById("imagem-principal").src="img/angelim frisada gr-19.png"
+    } else if (i == 5){
+    	document.getElementById("imagem-principal").src="img/bbb.png"
+    } else if (i == 6){
+    	document.getElementById("imagem-principal").src="img/bbb frisada.png"
+    } else if (i == 7){
+    	document.getElementById("imagem-principal").src="img/externa pinus.png"
+    } else if(i == 8){
+    	document.getElementById("imagem-principal").src="img/pintura.png"
+    } else if(i == 9){
+    	document.getElementById("imagem-principal").src="img/externa angelim.png"
+    } else if (i == 10){
+    	document.getElementById("imagem-principal").src="img/externa mista.png"
+    }
+
+}
