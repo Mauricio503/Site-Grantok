f6d623e0247db3d511d0a6b7de6e5a315513e746
Mauricio503 <mauriciogspagnol@gmail.com>
2017-11-29 16:46:42 -0200

    Produto tela inicial completo

--- Site grantok.com.br/js/troca-imagens.js
+++ Site grantok.com.br/js/troca-imagens.js
@@ -20,6 +20,62 @@ function mudaImagem(i) {
     	document.getElementById("imagem-principal").src="img/externa angelim.png"
     } else if (i == 10){
     	document.getElementById("imagem-principal").src="img/externa mista.png"
+    } else if (i == 11){
+    	document.getElementById("imagem-principal").src="img/mdf.png"
+    } else if (i == 12){
+    	document.getElementById("imagem-principal").src="img/mdf frisada gr-18.png"
+    } else if (i == 13){
+    	document.getElementById("imagem-principal").src="img/mdf gr-19.png"
+    } else if (i == 14){
+    	document.getElementById("imagem-principal").src="img/mdf gr-20.png"
+    } else if (i == 15){
+    	document.getElementById("imagem-principal").src="img/angelim gr-18.png"
+    } else if (i == 16){
+    	document.getElementById("imagem-principal").src="img/correr.png"
+    } else if (i == 17){
+    	document.getElementById("imagem-principal").src="img/pivotante.png"
+    } else if (i == 18){
+    	document.getElementById("imagem-principal").src="img/solida.png"
+    } else if (i == 19){
+    	document.getElementById("imagem-principal").src="img/bbb eucalipto puxador.png"
+    } else if (i == 20){
+    	document.getElementById("imagem-principal").src="img/bbb eucalipto fechadura.png"
+    } else if (i == 21){
+    	document.getElementById("imagem-principal").src="img/bbb eucalipto vigia.png"
+    } else if (i == 22){
+    	document.getElementById("imagem-principal").src="img/externa tauari.png"
+    } else if (i == 23){
+    	document.getElementById("imagem-principal-cabos").src="img/ancinho.png"
+    } else if (i == 24){
+    	document.getElementById("imagem-principal-cabos").src="img/enxadao.png"
+    } else if (i == 25){
+    	document.getElementById("imagem-principal-cabos").src="img/machado.png"
+    } else if (i == 26){
+    	document.getElementById("imagem-principal-cabos").src="img/martelo.png"
+    } else if (i == 27){
+    	document.getElementById("imagem-principal-cabos").src="img/picareta.png"
+    } else if (i == 28){
+    	document.getElementById("imagem-principal-cabos").src="img/cavadeira.png"
+    } else if (i == 29){
+    	document.getElementById("imagem-principal-cabos").src="img/enxada cabeçuda.png"
+    } else if (i == 30){
+    	document.getElementById("imagem-principal-cabos").src="img/enxada oval.png"
+    } else if (i == 31){
+    	document.getElementById("imagem-principal-cabos").src="img/enxada redondo.png"
+    } else if (i == 32){
+    	document.getElementById("imagem-principal-cabos").src="img/foice.png"
+    } else if (i == 33){
+    	document.getElementById("imagem-principal-cabos").src="img/marreta.png"
+    } else if (i == 34){
+    	document.getElementById("imagem-principal-cabos").src="img/pa curvo.png"
+    } else if (i == 35){
+    	document.getElementById("imagem-principal-cabos").src="img/pa reto.png"
+    } else if (i == 36){
+    	document.getElementById("imagem-principal-cabos").src="img/pa y.png"
+    } else if (i == 37){
+    	document.getElementById("imagem-principal-cabos").src="img/vassoura.png"
     }
 
 }
+
+
