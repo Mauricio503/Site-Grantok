77c394620708b91b8029be69353cb6c927f157eb
Mauricio503 <mauriciogspagnol@gmail.com>
2018-03-02 16:07:23 -0300

    alterando pagina para AMP

--- Site grantok.com.br v1.2 - Adequando a pesquisa google/js/troca-imagens.js
+++ Site grantok.com.br v1.2 - Adequando a pesquisa google/js/troca-imagens.js
@@ -1,79 +1,107 @@
 function mudaImagem(i) {    
 
 	if (i == 1) {
-        document.getElementById("imagem-principal").src="img/hdf branca.png";   
+        document.getElementById("imagem-principal").src="img/hdf branca.png";
+        document.getElementById("detalhes").href="hdf.html";   
     } else if (i == 2) {       
-    	document.getElementById("imagem-principal").src="img/hdf frisada.png";        
+    	document.getElementById("imagem-principal").src="img/hdf frisada.png";
+        document.getElementById("detalhes").href="hdf-frisada.html";         
     } else if (i == 3){
-    	document.getElementById("imagem-principal").src="img/angelim lisa.png"
+    	document.getElementById("imagem-principal").src="img/angelim lisa.png";
+        document.getElementById("detalhes").href="angelim.html";
     } else if (i == 4){
-    	document.getElementById("imagem-principal").src="img/angelim frisada gr-19.png"
+    	document.getElementById("imagem-principal").src="img/angelim frisada gr-19.png";
+        document.getElementById("detalhes").href="angelim-gr19.html";
     } else if (i == 5){
-    	document.getElementById("imagem-principal").src="img/bbb.png"
-    } else if (i == 6){
-    	document.getElementById("imagem-principal").src="img/bbb frisada.png"
+    	document.getElementById("imagem-principal").src="img/bbb.png";
+        document.getElementById("detalhes").href="bbb.html";
     } else if (i == 7){
-    	document.getElementById("imagem-principal").src="img/externa pinus.png"
+    	document.getElementById("imagem-principal").src="img/externa pinus.png";
+        document.getElementById("detalhes").href="externa-pinus.html";
     } else if(i == 8){
-    	document.getElementById("imagem-principal").src="img/pintura.png"
-    } else if(i == 9){
-    	document.getElementById("imagem-principal").src="img/externa angelim.png"
+    	document.getElementById("imagem-principal").src="img/pintura.png";
+        document.getElementById("detalhes").href="pintura.html";
     } else if (i == 10){
-    	document.getElementById("imagem-principal").src="img/externa mista.png"
+    	document.getElementById("imagem-principal").src="img/externa mista.png";
+        document.getElementById("detalhes").href="externa-mista.html";
     } else if (i == 11){
-    	document.getElementById("imagem-principal").src="img/mdf.png"
+    	document.getElementById("imagem-principal").src="img/mdf.png";
+        document.getElementById("detalhes").href="mdf.html";
     } else if (i == 12){
-    	document.getElementById("imagem-principal").src="img/mdf frisada gr-18.png"
+    	document.getElementById("imagem-principal").src="img/mdf frisada gr-18.png";
+        document.getElementById("detalhes").href="mdf-frisada-gr18.html";
     } else if (i == 13){
-    	document.getElementById("imagem-principal").src="img/mdf gr-19.png"
+    	document.getElementById("imagem-principal").src="img/mdf gr-19.png";
+        document.getElementById("detalhes").href="mdf-frisada-gr19.html";
     } else if (i == 14){
-    	document.getElementById("imagem-principal").src="img/mdf gr-20.png"
+    	document.getElementById("imagem-principal").src="img/mdf gr-20.png";
+        document.getElementById("detalhes").href="mdf-frisada-gr20.html";
     } else if (i == 15){
-    	document.getElementById("imagem-principal").src="img/angelim gr-18.png"
+    	document.getElementById("imagem-principal").src="img/angelim gr-18.png";
+        document.getElementById("detalhes").href="angelim-frisada-gr18.html";
     } else if (i == 16){
-    	document.getElementById("imagem-principal").src="img/correr.png"
+    	document.getElementById("imagem-principal").src="img/correr.png";
+        document.getElementById("detalhes").href="correr.html";
     } else if (i == 17){
-    	document.getElementById("imagem-principal").src="img/interna eucalipto.png"
-    } else if (i == 18){
-    	document.getElementById("imagem-principal").src="img/solida.png"
+    	document.getElementById("imagem-principal").src="img/interna eucalipto.png";
+        document.getElementById("detalhes").href="pintura-eucalipto.html";
     } else if (i == 19){
-    	document.getElementById("imagem-principal").src="img/bbb eucalipto puxador.png"
+    	document.getElementById("imagem-principal").src="img/bbb eucalipto puxador.png";
+        document.getElementById("detalhes").href="bbb-eucalipto-puxador.html";
     } else if (i == 20){
-    	document.getElementById("imagem-principal").src="img/bbb eucalipto fechadura.png"
+    	document.getElementById("imagem-principal").src="img/bbb eucalipto fechadura.png";
+        document.getElementById("detalhes").href="bbb-eucalipto-fechadura.html";
     } else if (i == 21){
-    	document.getElementById("imagem-principal").src="img/bbb eucalipto vigia.png"
+    	document.getElementById("imagem-principal").src="img/bbb eucalipto vigia.png";
+        document.getElementById("detalhes").href="bbb-eucalipto-vigia.html";
     } else if (i == 22){
-    	document.getElementById("imagem-principal").src="img/externa tauari.png"
+    	document.getElementById("imagem-principal").src="img/externa tauari.png";
+        document.getElementById("detalhes").href="externa-tauari.html";
     } else if (i == 23){
-    	document.getElementById("imagem-principal-cabos").src="img/ancinho.png"
+    	document.getElementById("imagem-principal-cabos").src="img/ancinho.png";
+        document.getElementById("detalhes-cabos").href="ancinho.html";
     } else if (i == 24){
-    	document.getElementById("imagem-principal-cabos").src="img/enxadao.png"
+    	document.getElementById("imagem-principal-cabos").src="img/enxadao.png";
+        document.getElementById("detalhes-cabos").href="enxadao.html";
     } else if (i == 25){
-    	document.getElementById("imagem-principal-cabos").src="img/machado.png"
+    	document.getElementById("imagem-principal-cabos").src="img/machado.png";
+        document.getElementById("detalhes-cabos").href="machado.html";
     } else if (i == 26){
-    	document.getElementById("imagem-principal-cabos").src="img/martelo.png"
+    	document.getElementById("imagem-principal-cabos").src="img/martelo.png";
+        document.getElementById("detalhes-cabos").href="martelo.html";
     } else if (i == 27){
-    	document.getElementById("imagem-principal-cabos").src="img/picareta.png"
+    	document.getElementById("imagem-principal-cabos").src="img/picareta.png";
+        document.getElementById("detalhes-cabos").href="picareta.html";
     } else if (i == 28){
-    	document.getElementById("imagem-principal-cabos").src="img/cavadeira.png"
+    	document.getElementById("imagem-principal-cabos").src="img/cavadeira.png";
+        document.getElementById("detalhes-cabos").href="cavadeira.html";
     } else if (i == 29){
-    	document.getElementById("imagem-principal-cabos").src="img/enxada cabeçuda.png"
+    	document.getElementById("imagem-principal-cabos").src="img/enxada cabeçuda.png";
+        document.getElementById("detalhes-cabos").href="enxada-cabecuda.html";
     } else if (i == 30){
-    	document.getElementById("imagem-principal-cabos").src="img/enxada oval.png"
+    	document.getElementById("imagem-principal-cabos").src="img/enxada oval.png";
+        document.getElementById("detalhes-cabos").href="enxada-oval.html";
     } else if (i == 31){
-    	document.getElementById("imagem-principal-cabos").src="img/enxada redondo.png"
+    	document.getElementById("imagem-principal-cabos").src="img/enxada redondo.png";
+        document.getElementById("detalhes-cabos").href="enxada-redondo.html";
     } else if (i == 32){
-    	document.getElementById("imagem-principal-cabos").src="img/foice.png"
+    	document.getElementById("imagem-principal-cabos").src="img/foice.png";
+        document.getElementById("detalhes-cabos").href="foice.html";
     } else if (i == 33){
-    	document.getElementById("imagem-principal-cabos").src="img/marreta.png"
+    	document.getElementById("imagem-principal-cabos").src="img/marreta.png";
+        document.getElementById("detalhes-cabos").href="marreta.html";
     } else if (i == 34){
-    	document.getElementById("imagem-principal-cabos").src="img/pa curvo.png"
+    	document.getElementById("imagem-principal-cabos").src="img/pa curvo.png";
+        document.getElementById("detalhes-cabos").href="pa-curvo.html";
     } else if (i == 35){
-    	document.getElementById("imagem-principal-cabos").src="img/pa reto.png"
+    	document.getElementById("imagem-principal-cabos").src="img/pa reto.png";
+        document.getElementById("detalhes-cabos").href="pa-reto.html";
     } else if (i == 36){
-    	document.getElementById("imagem-principal-cabos").src="img/pa y.png"
+    	document.getElementById("imagem-principal-cabos").src="img/pa y.png";
+        document.getElementById("detalhes-cabos").href="pa-y.html";
     } else if (i == 37){
-    	document.getElementById("imagem-principal-cabos").src="img/vassoura.png"
+    	document.getElementById("imagem-principal-cabos").src="img/vassoura.png";
+        document.getElementById("detalhes-cabos").href="vassoura.html";
     }
 
 }
